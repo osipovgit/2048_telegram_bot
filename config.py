0c29d701fee829baf4c48aa596fa73830390fa4c
Evgeny Osipov <47551687+osipovgit@users.noreply.github.com>
2020-10-06 22:40:31 +0700

    Delete new_field

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
 token_timekiller_bot = 'TELEGRAM_TOKEN'
 database_info = dict(database='', user='', password='', host='', port='')
-new_field = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
